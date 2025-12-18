6e29c5ed975f49f081bc4696d3cf2f65957f1979
bala-amypo <bala@amypo.in>
2025-12-18 09:23:26 +0000

    User commit

--- src/main/java/com/example/demo/model/DeliveryEvaluation.java
+++ src/main/java/com/example/demo/model/DeliveryEvaluation.java
@@ -62,31 +62,24 @@ public class DeliveryEvaluation {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Vendor getVendor() {
         return vendor;
     }
-
     public void setVendor(Vendor vendor) {
         this.vendor = vendor;
     }
-
     public SlaRequirement getSlaRequirement() {
         return slaRequirement;
     }
-
     public void setSlaRequirement(SlaRequirement slaRequirement) {
         this.slaRequirement = slaRequirement;
     }
-
     public Integer getActualDeliveryDays() {
         return actualDeliveryDays;
     }
-
     public void setActualDeliveryDays(Integer actualDeliveryDays) {
         this.actualDeliveryDays = actualDeliveryDays;
     }
@@ -94,34 +87,25 @@ public class DeliveryEvaluation {
     public Double getQualityScore() {
         return qualityScore;
     }
-
     public void setQualityScore(Double qualityScore) {
         this.qualityScore = qualityScore;
     }
-
     public LocalDate getEvaluationDate() {
         return evaluationDate;
     }
-
     public void setEvaluationDate(LocalDate evaluationDate) {
         this.evaluationDate = evaluationDate;
     }
-
     public Boolean getMeetsDeliveryTarget() {
         return meetsDeliveryTarget;
     }
-
     public void setMeetsDeliveryTarget(Boolean meetsDeliveryTarget) {
         this.meetsDeliveryTarget = meetsDeliveryTarget;
     }
-
     public Boolean getMeetsQualityTarget() {
         return meetsQualityTarget;
     }
-
     public void setMeetsQualityTarget(Boolean meetsQualityTarget) {
         this.meetsQualityTarget = meetsQualityTarget;
     }
-
-
 }
--- src/main/java/com/example/demo/model/SLARequirement.java
+++ src/main/java/com/example/demo/model/SLARequirement.java
@@ -37,4 +37,40 @@ public class SLARequirement {
             active = true;
         }
     }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getRequirementName() {
+        return requirementName;
+    }
+    public void setRequirementName(String requirementName) {
+        this.requirementName = requirementName;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Integer getMaxDeliveryDays() {
+        return maxDeliveryDays;
+    }
+    public void setMaxDeliveryDays(Integer maxDeliveryDays) {
+        this.maxDeliveryDays = maxDeliveryDays;
+    }
+    public Double getMinQualityScore() {
+        return minQualityScore;
+    }
+    public void setMinQualityScore(Double minQualityScore) {
+        this.minQualityScore = minQualityScore;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
