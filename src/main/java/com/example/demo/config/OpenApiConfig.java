6b40d8e869ef9703a98a4933672b6b938c677b9b
bala-amypo <bala@amypo.in>
2025-12-22 08:40:35 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,33 +1,20 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenAPIConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .info(new Info()
-                .title("Vendor SLA Performance Tracker API")
-                .version("1.0")
-                .description("API for managing vendor SLA performance tracking"))
-            .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-            .components(new Components()
-                .addSecuritySchemes("Bearer Authentication", 
-                    new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")));
-                        
-    }
-    .servers(List.of(
-                        new Server().url("https://9130.pro604cr.amypo.ai/")
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9175.pro604cr.amypo.ai/")
                 ));
+        }
 }
