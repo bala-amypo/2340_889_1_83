5eef7ce5ef024a9003f157f19bf713f00415cd78
bala-amypo <bala@amypo.in>
2025-12-25 17:00:57 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,13 +1,11 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -15,17 +13,8 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .addServersItem(new Server().url("https://9323.pro604cr.amypo.ai/").description("Development Server"))
-            .info(new Info()
-                .title("Vendor SLA Performance Tracker API")
-                .version("1.0")
-                .description("API for managing vendor SLA performance tracking"))
-            .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-            .components(new Components()
-                .addSecuritySchemes("Bearer Authentication", 
-                    new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")));
+                .servers(List.of(
+                        new Server().url("https://9323.pro604cr.amypo.ai/")
+                ));
     }
 }
